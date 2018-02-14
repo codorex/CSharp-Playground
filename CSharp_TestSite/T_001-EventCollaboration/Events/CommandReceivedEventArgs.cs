8e34b78e310b0adbc1614b23b0f924429a260125
KODAR\Momchil Momchilov <momchil@kodar.net>
2018-02-14 10:29:41 +0200

    - added updated project

---
+++ CSharp_TestSite/T_001-EventCollaboration/Events/CommandReceivedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T_001_EventCollaboration.Commands.Implementations;
+
+namespace T_001_EventCollaboration.Events
+{
+    public class CommandReceivedEventArgs : EventArgs
+    {
+        public DateTime DateReceived { get; set; }
+        public Command Command { get; set; }
+    }
+}
---
+++ CSharp_TestSite/T_001-EventCollaboration/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using T_001_EventCollaboration.Agents;
+using T_001_EventCollaboration.Commands.Implementations;
+using T_001_EventCollaboration.Events;
+using T_001_EventCollaboration.Processors;
+
+namespace T_001_EventCollaboration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Issuer issuer = new Issuer();
+            issuer.CommandIssued += issuer_onCommandIssued;
+
+            Task.WaitAll(new Task[]
+            {
+                issuer.Issue(new Command { DateIssued = DateTime.Now, Name = "First command" }),
+                issuer.Issue(new Command { DateIssued = DateTime.Now, Name = "Second command" }),
+                issuer.Issue(new Command { DateIssued = DateTime.Now, Name = "Third command" })
+            });
+
+            Console.ReadKey();
+        }
+
+        public static void issuer_onCommandIssued(object sender, CommandIssuedEventArgs eventArgs)
+        {
+            Console.WriteLine(eventArgs.Issuer.GetType());
+            Console.WriteLine($"--issued command \"{eventArgs.Command.Name}\"; \n--issued by {sender}");
+            Console.WriteLine();
+        }
+    }
+}
